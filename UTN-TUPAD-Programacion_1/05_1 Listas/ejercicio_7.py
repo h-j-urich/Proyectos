b4d7cb1a4a2149743dddeb09e6edd57cf32bea68
Hernan Jorge Urich <hernan.urich@gmail.com>
2025-04-24 15:53:39 -0300

    se modifico directorio

--- UTN-TUPAD-Programacion_1/05_1 Listas/ejercicio_7.py
+++
@@ -1,9 +0,0 @@
-""" 7) Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores 
-cualesquiera."""
-
-autos = ["sedan", "polo", "suran", "gol"] 
-
-autos[1:3] = ["fiesta", "corolla"]
-
-print(autos)
-
